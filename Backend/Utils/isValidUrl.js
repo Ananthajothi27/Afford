05ea0a67d250bfc654e0e6743a43c56c2156e240
Ananthajothi27 <2312045@nec.edu.in>
2026-07-02 05:49:59 +0530

    Backend files added

---
+++ Backend/Utils/isValidUrl.js
@@ -0,0 +1,10 @@
+export const isValidUrl = (url) =>{
+    try{
+        const parseurl = new URL(url);
+        return parseurl.protocol === 'http:' || parseurl.protocol === 'https:';
+    }
+    catch(error)
+    {
+        return false;
+    }
+}
